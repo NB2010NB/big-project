0043049ce4f002ea3d109b2d7133c259fdb02247
NB2010NB <natanbalai1@gmail.com>
2022-07-31 09:13:50 +0300

    big project

---
+++ JS/phone.js
@@ -0,0 +1,31 @@
+const card = document.getElementById("cards");
+const photoPhone= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG7BHzDERlveMVsFMt_mMP0wOTeeajYHwP1SZDNgNLXK82xbGJTpafli1GQowXoS_jPjU&usqp=CAU" 
+const url = "https://my-json-server.typicode.com/Jeck99/fake-server/devices"
+async function devices() {
+    const res = await fetch(url)
+    const data = await res.json()
+    return data
+}
+async function createCards() {
+    let phones = await devices()
+    card.innerHTML = phones.map((item) => {
+        console.log(item);
+return `<div class="card" style="width: 18rem ;">
+  <img class="card-img-top" src="${photoPhone}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">
+    brand: ${item.brand}<br>
+    createdAt: ${item.createdAt}<br>
+    id: ${item.id}<br>
+    isAvailable: ${item.isAvailable}<br>
+    price: ${item.price}$<br>
+    color: ${item.color}
+    </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+    })
+}
+createCards()
+
---
+++ JS/users.js
@@ -0,0 +1,44 @@
+const url = "https://my-json-server.typicode.com/Jeck99/fake-server/users"
+const usersPhoto = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdRUJULNIXjikp0FIZKfI51_wc_-YhQo3BQw&usqp=CAU"
+const usersCards = document.getElementById("cardsUsers");
+async function allData() {
+    const data = await fetch(url)
+    const allData = await data.json()
+    return allData;
+}
+let photoArrayUsers = [
+    'user.jfif',
+    'user1.jfif',
+    'user2.jfif',
+    'user3.jfif',
+    'user4.jfif',
+    'user5.jfif',
+    'user6.jfif',
+    'user7.jfif',
+    'user8.jfif',
+    'user9.jfif',
+]
+function arrayPhotoRun(){
+    
+}
+async function createUsers() {
+    const users = await allData()
+    usersCards.innerHTML += users.map((item) => {
+        console.log(users);
+        return `<div class="card" style="width: 18rem ;">
+    <img class="card-img-top" src="${usersPhoto}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="Friends-card">Friends card</h5>
+      <p class="card-text">
+      first name: ${item.name.first}<br>
+      age: ${item.age}<br>
+      email: ${item.email}<br>
+      phone: ${item.phone}$<br>
+      id: ${item._id}
+      </p>
+      <a href="#" class="btn btn-primary">Click to send a message</a>
+    </div>
+  </div>`
+    })
+}
+createUsers()
